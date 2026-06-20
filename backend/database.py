e9607d7df7510971ae7c0d1ec1741ed43208d98c
TheRepairGuyTT <151693218+therepairguytt@users.noreply.github.com>
2026-06-20 06:22:02 -0400

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -18,7 +18,7 @@ if DB_USER and DB_PASSWORD:
 else:
     DATABASE_URL = "sqlite:///database.db"
 
-engine = create_engine(DATABASE_URL, echo=False)
+engine = create_engine(DATABASE_URL, echo=False, pool_size=200, max_overflow=0)
 
 def get_session():
     with Session(engine) as session:
