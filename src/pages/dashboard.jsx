82a7c8d925c6cc47ff36426b311c43a8a117d150
TheRepairGuyTT <151693218+therepairguytt@users.noreply.github.com>
2026-06-29 03:16:52 -0400

    Update dashboard.jsx

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -236,11 +236,9 @@ export default function Dashboard() {
                           </div>
                         </td>
                         <td className="p-4 text-center">
-                          <div className="font-bold text-slate-200">
-                            <a className="cursor-pointer hover:text-white hover:bg-indigo-800 hover:font-bold hover:underline hover:rounded transition-all ml-1" href={`http://${ctrl.ip_address}`} target="_blank" rel="noopener noreferrer">
-                            {ctrl.ip_address} <ExternalLink size={10}/>
+                            <a className="inline-flex items-center gap-1 cursor-pointer hover:text-white hover:bg-indigo-800 hover:font-bold hover:underline hover:rounded transition-all ml-1 px-1" href={`http://${ctrl.ip_address}`} target="_blank" rel="noopener noreferrer">
+                              {ctrl.ip_address} <ExternalLink size={12}/>
                             </a>
-                          </div>
                         </td>
                         <td className="p-4 text-center">
                           <span className="px-2 py-1 bg-slate-800 rounded text-[10px] font-bold text-indigo-400 border border-slate-700 uppercase">
