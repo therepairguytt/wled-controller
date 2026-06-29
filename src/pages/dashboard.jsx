5c5c504bf132f88ba9b849645e3cf58545f104c4
TheRepairGuyTT <151693218+therepairguytt@users.noreply.github.com>
2026-06-29 03:04:35 -0400

    Update dashboard.jsx

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -238,7 +238,7 @@ export default function Dashboard() {
                         <td className="p-4 text-center">
                           <div className="font-bold text-slate-200">
                             <a className="cursor-pointer hover:text-white hover:bg-indigo-800 hover:font-bold hover:underline hover:rounded transition-all ml-1" href={`http://${ctrl.ip_address}`} target="_blank" rel="noopener noreferrer">
-                            {ctrl.ip_address} <ExternalLink size={5}/>
+                            {ctrl.ip_address} <ExternalLink size={10}/>
                             </a>
                           </div>
                         </td>
